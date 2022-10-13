deed0c0e9de9a2d54bd7128894d75a674189f1b8
spandemic <anloeng@ucsc.edu>
2022-10-13 12:36:35 -0700

    player model

--- main.js
+++ main.js
@@ -31,11 +31,31 @@ options = {
 /**
  * @typedef {{
  * pos: Vector,
- * }}
+ * state: number
+ * }} Player
  */
+
+/**
+ * @type { Player [] }
+ */ 
+let player;
 
 function update() {
   if (!ticks) {
-
+    player = [];
+    for (let i = 0; i < 4; i++) {
+        for (let k = 0; k < 4; k++) {
+            const pos = vec((G.WIDTH/2 - 6) + (k * 6), (G.HEIGHT/2 + 42) + (i * 6));
+            const state = 0;
+            player.push({ pos, state });
+        }
+    }
   }
+
+  remove(player, (p) => {
+    if (p.state === 0) {
+        color("blue");
+    }
+    char("a", p.pos);
+  })
 }
