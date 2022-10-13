a1d0b2ebc9c133cc62618f26b4bfcb87cfc8cd3d
spandemic <anloeng@ucsc.edu>
2022-10-13 12:11:54 -0700

    update

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 title = "Car Game";
 
 description = `
+
 `;
 
-characters = [];
+const G = {
+    WIDTH: 200,
+    HEIGHT: 200
+};
 
-options = {};
+characters = [
+`
+gggggg
+gggggg
+gggggg
+gggggg
+gggggg
+gggggg
+`
+];
+
+options = {
+    viewSize: {
+        x: G.WIDTH,
+        y: G.HEIGHT
+    },
+    theme: "dark"
+};
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * }}
+ */
 
 function update() {
   if (!ticks) {
+
   }
 }
