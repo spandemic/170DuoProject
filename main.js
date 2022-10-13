74901384869be00af19d2096ca38b87b8453600e
andib7 <andibarron7@gmail.com>
2022-10-13 12:49:36 -0700

    Basic Obstacle Movement

--- main.js
+++ main.js
@@ -19,10 +19,11 @@ gggggg
 gggggg
 gggggg
 `,`
-lcccl
-cclcc
-l   l
-c   c
+ gggg
+gggggg
+gg  gg
+gg  gg
+gg  gg
 `
 ];
 
@@ -41,8 +42,40 @@ options = {
  * }} Player
  */
 
+/**
+ * @type { Player [] }
+ */ 
+let player;
+
+let obstacle;
+
 function update() {
   if (!ticks) {
-
+    player = [];
+    for (let i = 0; i < 4; i++) {
+        for (let k = 0; k < 4; k++) {
+            const pos = vec((G.WIDTH/2 - 6) + (k * 6), (G.HEIGHT/2 + 42) + (i * 6));
+            const state = 0;
+            player.push({ pos, state });
+        }
+    }
+    obstacle = {
+      pos: vec(20, 20)
+    }
   }
+
+  if(obstacle.pos.y < G.HEIGHT){
+    obstacle.pos.y += G.OBSTACLESPEED;
+    obstacle.pos.x += G.OBSTACLESPEED;
+  }
+
+  color("red");
+  char("b", obstacle.pos);
+
+  remove(player, (p) => {
+    if (p.state === 0) {
+        color("blue");
+    }
+    char("a", p.pos);
+  })
 }
