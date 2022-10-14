42a23e9c9edf3f635e5d78508d68e32cdb561803
spandemic <anloeng@ucsc.edu>
2022-10-13 20:24:32 -0700

    working lane changes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-title = "OFF-DUTY COP";
+title = "SWERVE";
 
 description = `
 [HOLD] to Brake
+[RELEASE] to Swerve
 `;
 
 const G = {
@@ -49,7 +50,7 @@ options = {
 let player;
 
 /**
- * @typedef {{ pos: Vector, type: number
+ * @typedef {{ pos: Vector,
  * }} Obstacle
  */
 
@@ -69,10 +70,12 @@ let obstacle;
  * @type { StreetLine []}  
  */
 let streetLine;
-let lineGroup;
+
 let diffUp;
 let playerPosition;
 let playerX;
+let obstacleSpawn;
+let moving;
 
 function update() {
   if (!ticks) {
@@ -80,39 +83,35 @@ function update() {
     player = [];
     playerPosition = 1;
     playerX = 0;
-    // needlessly complicated street line generation
+    obstacleSpawn = 0;
+    moving = 1;
+  
     streetLine = [];
-    lineGroup = [];
     // create group of streetLine
-    for (let n = 0; n < 5; n++) {
+    for (let n = 0; n < 3; n++) {
         const drawPos = 1 * (n * 72);   // distance between each draw
-        // draw a 2x6 array that represents a street line
-        for (let i = 0; i < 2; i++) {
-            for (let k = 0; k < 6; k++) {
-                const pos = vec((G.WIDTH/2 + 1) + (i * G.STREETLINE_SIZE), (drawPos - (6 * G.STREETLINE_SIZE)) + (k * G.STREETLINE_SIZE));
-                streetLine.push({ pos });
-            }
-        }
-        // adds the entire line into one element of lineGroup
-        lineGroup.push(streetLine);
+        let pos = vec((G.WIDTH/3 + 12), (-drawPos - (28)));
+        streetLine.push({ pos });
+        pos = vec((G.WIDTH * 2/3 - 6), (-drawPos - (28)));
+        streetLine.push({ pos });
     }
     obstacle = [];
     }
 
-    // addScore(floor(2 * G.OBSTACLESPEED));
+    addScore(floor(2 * floor(G.OBSTACLESPEED / 2)));
 
     diffUp--;
-    if (diffUp < 0 && G.OBSTACLESPEED < 7 && !input.isPressed) {
-        G.OBSTACLESPEED += 0.5;
+    if (diffUp < 0 && G.OBSTACLESPEED < 9 && !input.isPressed) {
+        G.OBSTACLESPEED += 0.75;
         diffUp = 10;
         console.log(G.OBSTACLESPEED);
     }
 
     if(input.isPressed) {
-        if (G.OBSTACLESPEED < 0.16) {
+        if (G.OBSTACLESPEED < 0.11) {
             G.OBSTACLESPEED -= 0.01;
         } else {
-            G.OBSTACLESPEED -= 0.15;
+            G.OBSTACLESPEED -= 0.1;
         }
     }
 
@@ -123,9 +122,17 @@ function update() {
     if(input.isJustReleased) {
         player = [];
         if (playerPosition === 2) {
-            playerPosition = 0;
-        } else {
+            playerPosition--;
+            moving = -1;
+        } else if (playerPosition === 0) {
             playerPosition++;
+            moving = 1;
+        } else {
+            if (moving < 0) {
+                playerPosition--;
+            } else {
+                playerPosition++;
+            }
         }
     }
 
@@ -136,18 +143,17 @@ function update() {
     box(G.WIDTH, G.HEIGHT / 2, G.WIDTH / 4, G.HEIGHT);
 
   // iterates through each array of sprites in streetLine
-  remove(lineGroup, (l) => {
-    // retrieves pos from each sprite in streetLine
-    for (let i = 0; i < l.length; i++) {
-        l[i].pos.y += G.OBSTACLESPEED / 3; // speed of line relative to obstacleSpeed
+  remove(streetLine, (l) => {
+    // retrieves pos from each sprite in streetLine 
         color("yellow");
-        box(l[i].pos, G.STREETLINE_SIZE); // draw sprite
+        box(l.pos, 8, 40); // draw sprite
+        l.pos.y += G.OBSTACLESPEED;
 
         // cycles lines from bottom of screen to top
-        if(l[i].pos.y > G.HEIGHT) {
-            l[i].pos.y = -24;
+        if(l.pos.y > G.HEIGHT) {
+            l.pos.y = -24;
         }
-    }
+    
     });
     
     if(player.length === 0) {
@@ -155,9 +161,9 @@ function update() {
         if (playerPosition === 1) {
             playerX = 91;
         } else if (playerPosition === 0) {
-            playerX = 46
+            playerX = 40;
         } else if (playerPosition === 2) {
-            playerX = 136
+            playerX = 142;
         }
         for (let i = 0; i < 8; i++) {
             for (let k = 0; k < 5; k++) {
@@ -193,24 +199,27 @@ function update() {
     
 
     if (obstacle.length === 0) {
-        let posX = G.WIDTH/2;
-        let posY = -rnd(400, 1000);
-        let type = floor(rnd(0, 2));
-        obstacle.push({ pos: vec(posX, posY), type });
-         posX = G.WIDTH/2;
-         posY = -rnd(1000, 2000);
-         type = floor(rnd(0, 2)); 
-        obstacle.push({ pos: vec(posX, posY), type });
+        for (let i = 0; i < 4; i++) {
+            obstacleSpawn = floor(rnd(0, 3));
+            let posX = 0;
+            if (obstacleSpawn === 0) {
+                posX = G.WIDTH - 54;
+            } else if (obstacleSpawn === 1) {
+                posX = G.WIDTH/2;
+            } else {
+                posX = G.WIDTH/2 + 54
+            }
+            const posY = rnd(400, 1800);
+            obstacle.push({pos: vec(posX, posY)})
+        }
     }
 
     remove(obstacle, (o) => {
-        o.pos.y += (G.OBSTACLESPEED);
-        
-        color(o.type === 0 ? "green" : "red");
+        color("red");
         box(o.pos, G.WIDTH, 8);
 
+        o.pos.y += (G.OBSTACLESPEED);
         return(o.pos.y > G.HEIGHT);
-        
     });
 
     
